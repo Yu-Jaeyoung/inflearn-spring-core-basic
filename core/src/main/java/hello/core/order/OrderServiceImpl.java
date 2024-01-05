34fe8912c498f257eb3df5baf7d98f1929235c2c
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2024-01-05 22:59:19 +0900

    Section 7. 의존관계 자동 주입
    - 다양한 의존관계 주입 방법
    - 옵션 처리
    - 생성자 주입을 선택하라!
    - 롬복과 최신 트랜드
    - 조회 빈이 2개 이상 - 문제
    - @Autowired 필드 명, @Qualifier, @Primary
    - 애노테이션 직접 만들기
    - 조회한 빈이 모두 필요할 때, List, Map
    - 자동, 수동의 올바른 실무 운영 기준

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,36 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+// @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    // 생성자가 하나라면, Autowired가 자동으로 붙는다. (생략 가능)
+    // @RequiredArgsConstructor 는 final(required) 필드 값을 기반으로 생성자를 만든다.
+    // 즉, 생성자 부분 작성 생략이 가능하다.
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -3,7 +3,9 @@ package hello.core.scan;
 import static org.assertj.core.api.Assertions.*;
 
 import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -14,5 +16,9 @@ public class AutoAppConfigTest {
 
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
